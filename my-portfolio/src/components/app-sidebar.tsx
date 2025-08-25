95374b29986f94a0ecf0e52834c8acc2b1c88939
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-08-25 21:13:06 +0700

    add sidebar

---
+++ my-portfolio/src/components/app-sidebar.tsx
@@ -0,0 +1,75 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import Link from "next/link";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SideBarProfile,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "About",
+    url: "/about",
+    icon: Inbox,
+  },
+  {
+    title: "Archievement",
+    url: "/archievement",
+    icon: Calendar,
+  },
+  {
+    title: "Projects",
+    url: "/projects",
+    icon: Search,
+  },
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: Settings,
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SideBarProfile />
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
