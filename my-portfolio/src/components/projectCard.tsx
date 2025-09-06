163dcfca8e5b1b4003962c891bddf61f717137a8
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-09-06 21:15:59 +0700

    add project card

---
+++ my-portfolio/src/components/projectCard.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Image from 'next/image'
+import { StaticImageData } from 'next/image'
+import Link from 'next/link'
+
+interface PorjectCardProps {
+    image: string | StaticImageData;
+    title: string;
+    description: string;
+    icons: { src: string; alt: string }[]
+}
+
+const ProjectCard = ({ image, title, description, icons }: PorjectCardProps) => {
+    const slug = title.toLowerCase().replace(/\s+/g, "-");
+  return (
+    <div>
+      <div className='w-72 flex flex-col border border-gray-300 rounded-lg overflow-hidden'>
+        <div className="aspect-[2/1] group relative w-full hover:">
+          <Image 
+            src={image}
+            alt="lol"
+            fill
+            className="object-contain"
+          />
+          
+          <Link
+            href={`/projects/${slug}`}
+            className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center text-white"
+          >
+            <span className="text-xs">View Project</span>
+          </Link>
+        </div>
+
+        <div className='h-1/2 p-3 space-y-1'>
+            <p className='text-sm font-medium'>{title}</p>
+            <p className='text-xs text-neutral-500'>{description}</p>
+            <div className='flex flex-wrap gap-2 mt-2'>
+              {icons.map((icon, index) => (
+                <img
+                    key={index}
+                    src={icon.src}
+                    alt={icon.alt}
+                    width={25}
+                    height={25}
+                />
+              ))}
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard
