96c137b3c9dc7cd6b82f67fc01eb6187934f1d18
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-09-19 18:35:17 +0700

    Update project page

--- my-portfolio/src/app/projects/[slug]/page.tsx
+++ my-portfolio/src/app/projects/[slug]/page.tsx
@@ -3,11 +3,13 @@
 import React from 'react'
 import { projects } from '../page'
 import { FaGithub } from "react-icons/fa";
+import { CircleArrowLeft } from 'lucide-react';
 import Image from 'next/image';
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm";
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
+import Link from 'next/link';
 
 const page = () => {
     const { slug } = useParams() as { slug: string };
@@ -35,15 +37,31 @@ const page = () => {
   return (
     <div>
         <div>
+            <Link href="/projects" className="text-sm cursor-pointer flex items-center gap-2 mb-2">
+                <CircleArrowLeft size={16} />
+                Back
+            </Link>
+
             <p className='font-bold text-lg'>{project.title}</p>
-            <p className='text-sm'>{project.description}</p>
+            <p className='text-sm text-neutral-600 dark:text-neutral-400'>{project.description}</p>
 
             <hr className='my-4'/>
 
             <div className='flex justify-between'>
                 <div className='flex gap-2 text-sm'>Tech Stack: 
                     {project.icons.map((icon, i) => (
-                        <img key={i} src={icon.src} alt={icon.alt} width={25} height={25}/>
+                        <div key={i} className="relative group flex justify-center">
+                            <img 
+                                src={icon.src} 
+                                alt={icon.alt} 
+                                width={25} 
+                                height={25}
+                                className={`transition-transform duration-200 group-hover:scale-110 ${icon.type === "mono" ? "dark:invert" : "" }`}
+                            />
+                            <span className="absolute top-[-1.5rem] px-2 py-1 text-xs text-white bg-gray-800 rounded-md opacity-0 group-hover:opacity-100 group-hover:translate-y-[-5px] transition-all duration-200 whitespace-nowrap">
+                                {icon.alt}
+                            </span>
+                        </div>
                     ))}
                 </div>
 
--- my-portfolio/src/components/projectCard.tsx
+++ my-portfolio/src/components/projectCard.tsx
@@ -4,22 +4,22 @@ import { StaticImageData } from 'next/image'
 import Link from 'next/link'
 
 interface PorjectCardProps {
+    slug: string;
     image: string | StaticImageData;
     title: string;
     description: string;
-    icons: { src: string; alt: string }[]
+    icons: { src: string; alt: string; type: string }[]
 }
 
-const ProjectCard = ({ image, title, description, icons }: PorjectCardProps) => {
-    const slug = title.toLowerCase().replace(/\s+/g, "-");
+const ProjectCard = ({ slug, image, title, description, icons }: PorjectCardProps) => {
+  
   return (
     <div>
-      <div className='w-72 flex flex-col border border-gray-300 rounded-lg overflow-hidden'>
+      <div className='w-96 flex flex-col border border-gray-300 rounded-lg overflow-hidden transition-all duration-300 hover:scale-[1.02] hover:shadow-xl hover:border-gray-400'>
         <div className="aspect-[2/1] group relative w-full hover:">
           <Image 
             src={image}
-            alt="lol"
-            fill
+            alt={title}
             className="object-contain"
           />
           
@@ -27,13 +27,13 @@ const ProjectCard = ({ image, title, description, icons }: PorjectCardProps) => 
             href={`/projects/${slug}`}
             className="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center text-white"
           >
-            <span className="text-xs">View Project</span>
+            <span className="text-xs">View Project →</span>
           </Link>
         </div>
 
         <div className='h-1/2 p-3 space-y-1'>
             <p className='text-sm font-medium'>{title}</p>
-            <p className='text-xs text-neutral-500'>{description}</p>
+            <p className='text-xs text-neutral-500 dark:text-neutral-400'>{description}</p>
             <div className='flex flex-wrap gap-2 mt-2'>
               {icons.map((icon, index) => (
                 <img
@@ -42,6 +42,7 @@ const ProjectCard = ({ image, title, description, icons }: PorjectCardProps) => 
                     alt={icon.alt}
                     width={25}
                     height={25}
+                    className={`${icon.type === "mono" ? "dark:invert" : "" }`}
                 />
               ))}
             </div>
